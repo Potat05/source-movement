ed14a52bffca98b25707317a0dae9d12f857c4ce
Potat05 <williamkoerber6@gmail.com>
2023-12-12 16:55:51 -0600

    ambient light

--- src/lib/game/level.ts
+++ src/lib/game/level.ts
@@ -1,4 +1,4 @@
-import { CubeTextureLoader, Scene } from "three";
+import { AmbientLight, Color, CubeTextureLoader, DirectionalLight, Mesh, MeshStandardMaterial, Scene } from "three";
 // @ts-ignore - TODO: Why is this import all fucked?
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 // @ts-ignore - TODO: Why is this import all fucked?
@@ -24,6 +24,7 @@ export class Level {
         const info: {
             scene: 'scene.gltf';
             skybox?: [ string, string, string, string, string, string ];
+            ambient?: { r: number; g: number; b: number; intensity: number; };
         } = await (await fetch(`${path}/info.json`)).json();
 
         const gltf = await new GLTFLoader().loadAsync(`${path}/${info.scene}`);
@@ -31,6 +32,18 @@ export class Level {
 
         const scene = new Scene();
         scene.add(group);
+
+        // TODO: Maybe do this in the actual .blend scene instead?
+        // MeshStandardMaterial metalness is 1 by default that leads to ambient light not working.
+        scene.traverse(child => {
+            if(!(child instanceof Mesh)) return;
+            if(!(child.material instanceof MeshStandardMaterial)) return;
+            child.material.metalness = 0;
+        });
+
+        if(info.ambient) {
+            scene.add(new AmbientLight(new Color(info.ambient.r, info.ambient.g, info.ambient.b), info.ambient.intensity));
+        }
 
         if(info.skybox) {
             scene.background = await new CubeTextureLoader()
